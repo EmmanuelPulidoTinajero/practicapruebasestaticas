b9cdc8a5c5cf081a8afbe8c870e9ebd8a6fb10d5
emmanuel pulido <jepulidotinajero@gmail.com>
2026-04-06 12:43:16 -0600

    feat: case 3 calculator

--- tdd/stringcalculator.py
+++ tdd/stringcalculator.py
@@ -3,4 +3,8 @@ class StringCalculator:
         if not numbers:
             return 0
         return sum(map(int, numbers.split(',')))
-    
+    def add(self, numbers):
+        if not numbers:
+            return 0
+        normalized_numbers = numbers.replace('\n', ',')
+        return sum(map(int, normalized_numbers.split(',')))
