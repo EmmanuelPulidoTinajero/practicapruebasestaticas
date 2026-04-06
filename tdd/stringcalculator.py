2c60ccab60c020c4dcc8bc5951aac55d22ca43f9
emmanuel pulido <jepulidotinajero@gmail.com>
2026-04-06 12:47:44 -0600

    feat: case 4

--- tdd/stringcalculator.py
+++ tdd/stringcalculator.py
@@ -2,9 +2,7 @@ class StringCalculator:
     def add(self, numbers):
         if not numbers:
             return 0
-        return sum(map(int, numbers.split(',')))
-    def add(self, numbers):
-        if not numbers:
-            return 0
         normalized_numbers = numbers.replace('\n', ',')
+        if normalized_numbers.endswith(','):
+            raise ValueError("Invalid input: separator at the end is not allowed")
         return sum(map(int, normalized_numbers.split(',')))
