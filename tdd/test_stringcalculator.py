041b6eac9cca4a967a9e3a30996a9d13a42e33f1
emmanuel pulido <jepulidotinajero@gmail.com>
2026-04-06 12:39:31 -0600

    test: case 3 added

--- tdd/test_stringcalculator.py
+++ tdd/test_stringcalculator.py
@@ -11,9 +11,11 @@ class TestStringCalculator(unittest.TestCase):
     def test_single_number_returns_that_number(self):
         self.assertEqual(self.calculator.add("1"), 1)
 
+        def test_new_lines_as_separators(self):
+        self.assertEqual(self.calculator.add("1\n2\n3"), 6)
+
     def test_two_numbers_returns_sum(self):
         self.assertEqual(self.calculator.add("1,2"), 3)
 
     def test_multiple_numbers_returns_sum(self):
         self.assertEqual(self.calculator.add("1,2,3"), 6)
-    
