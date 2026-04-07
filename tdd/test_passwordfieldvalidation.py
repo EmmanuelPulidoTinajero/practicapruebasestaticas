99e0d8304cb63e35b3817781e8940ef76b749bd7
emmanuel pulido <jepulidotinajero@gmail.com>
2026-04-07 16:52:26 -0600

    test: cases 4 and 5

--- tdd/test_passwordfieldvalidation.py
+++ tdd/test_passwordfieldvalidation.py
@@ -29,3 +29,22 @@ class TestPasswordFieldValidation(unittest.TestCase):
         self.assertFalse(result['valid'])
         self.assertIn("Password must be at least 8 characters", result['errors'])
         self.assertIn("The password must contain at least 2 numbers", result['errors'])
+        def test_password_missing_capital_letter(self):
+            result = self.validator.validate("validpass12")
+            self.assertFalse(result['valid'])
+            self.assertIn("password must contain at least one capital letter", result['errors'])
+
+        def test_password_with_capital_letter(self):
+            result = self.validator.validate("Validpass12")
+            self.assertTrue(result['valid'])
+            self.assertEqual([], result['errors'])
+
+        def test_password_missing_special_character(self):
+            result = self.validator.validate("Validpass12")
+            self.assertFalse(result['valid'])
+            self.assertIn("password must contain at least one special character", result['errors'])
+
+        def test_password_with_special_character(self):
+            result = self.validator.validate("Validpass12!")
+            self.assertTrue(result['valid'])
+            self.assertEqual([], result['errors'])
